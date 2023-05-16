cfb667b82db46812cac8c3ca1fd4614c38242775
plpabla <65901051+plpabla@users.noreply.github.com>
2023-05-16 21:51:31 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Hello world</title>
+</head>
+<body>
+  <h1>Hello there</h1>
+  <?php
+  echo "Hello from PHP";
+?>
+
+</body>
+</html>
