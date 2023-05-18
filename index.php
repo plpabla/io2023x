624f6198e3102a658dd515fb9b81e2b8fab68350
pawel-laptop <p.blaszczy@gmail.com>
2023-05-18 20:27:50 +0200

    test js i poprawa formatowania

--- index.php
+++ index.php
@@ -1,18 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-  <meta charset="utf-8">
-  <title>Hello there</title>
+    <meta charset="utf-8">
+    <title>Hello there</title>
 </head>
+
 <body>
-  <h1>Hello there</h1>
-  <?php
+    <h1>Hello there</h1>
+    <?php
   echo "Hello from PHP";
 ?>
-<br>
-Ręczne uruchomienie skryptu
-  <br> <b>Ostatni test:</b>: to z automatu powinno za godzinę się pojawić... 
-<br><br>
-eh, to jeszcze z poziomu VS Code i mojego "CD" na szybko xD
+    <br>
+    Ręczne uruchomienie skryptu
+    <br> <b>Ostatni test:</b>: to z automatu powinno za godzinę się pojawić...
+    <br><br>
+    eh, to jeszcze z poziomu VS Code i mojego "CD" na szybko xD
+    <script>
+    alert("A JS bangla?")
+    </script>
 </body>
+
 </html>
