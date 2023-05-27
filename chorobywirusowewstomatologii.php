a4275a27a79629b875e6bdcaf642718acbe00fc0
puellamiranda <kordekewa@gmail.com>
2023-05-27 21:20:56 +0200

    Dodanie formularza do dodawania chorób

--- chorobywirusowewstomatologii.php
+++ chorobywirusowewstomatologii.php
@@ -9,8 +9,8 @@
 <body>
     wrr brr 
     <?php
-    $dbconn = pg_connect("host=localhost dbname=jednostka_chorobowa user=moj_uzytkownik password=moje_haslo");
-    $query = "SELECT * FROM jednostka_chorobowa";
+    $dbconn = pg_connect("host=localhost dbname=choroby user=moj_uzytkownik password=moje_haslo");
+    $query = "SELECT * FROM choroby";
     $result = pg_query($dbconn, $query);
     
     echo "<table>";
@@ -27,6 +27,39 @@
     }
     echo "</table>";
     
+    pg_close($dbconn);
+    ?>
+
+//formularz dodawania chorób
+
+<?php
+    $dbconn = pg_connect("host=localhost dbname=choroby user=moj_uzytkownik password=moje_haslo");
+    $query = "SELECT * FROM choroby";
+    $result = pg_query($dbconn, $query);
+    //
+    <form action="dodaj_chorobe.php" method="POST">
+   
+<label for="pole1">Jednostka chorobowa:</label>
+<input type="text" id="pole1" name="pole1" required>
+
+<label for="pole2">Czynnik etiologiczny:</label>
+<input type="text" id="pole2" name="pole2" required>
+
+<label for="pole3">Objawy ogólne i miejscowe poza jamą ustną:</label>
+<input type="text" id="pole3" name="pole3" required>
+
+<label for="pole4">Objawy miejscowe w jamie ustnej:</label>
+<input type="text" id="pole4" name="pole4" required>
+
+<label for="pole5">Rozpoznanie:</label>
+<input type="text" id="pole5" name="pole5" required>
+
+<label for="pole6">Różnicowanie:</label>
+<input type="text" id="pole6" name="pole6" required>
+
+    <button type="submit">Dodaj nową jednostkę chorobową</button>
+  </form>
+      
     pg_close($dbconn);
     ?>
 </body>
---
+++ dodaj_chorobe.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>Dodawanie jednostki chorobowej</title>
+</head>
+
+<body>
+       <?php
+    $dbconn = pg_connect("host=localhost dbname=choroby user=moj_uzytkownik password=moje_haslo");
+   
+    $pole1 = $_POST['pole1'];
+    $pole2 = $_POST['pole2'];
+    $pole3 = $_POST['pole3'];
+    $pole4 = $_POST['pole4'];
+    $pole5 = $_POST['pole5'];
+    $pole6 = $_POST['pole6'];
+
+    $query = "INSERT INTO choroby (jednostka_chorobowa, czynnik_etiologiczny, objawy_ogolne_i_miejscowe_poza_ju, objawy_miejscowe_w_ju, rozpoznanie, roznicowanie) VALUES ('$pole1', '$pole2', '$pole3', '$pole4', '$pole5', '$pole6')";
+    
+    $result = pg_query($dbconn, $query);
+
+    
+    pg_close($dbconn);
+    ?>
+</body>
+
+</html>
