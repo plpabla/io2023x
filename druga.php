cdb81743ff2be466d759f263f77ae5e5782c8cb7
puellamiranda <kordekewa@gmail.com>
2023-05-27 20:10:33 +0200

    Dodanie pustej tabeli chorób (a przynajmniej taki był zamiar)

--- druga.php
+++ druga.php
@@ -3,13 +3,31 @@
 
 <head>
     <meta charset="utf-8">
-    <title>there</title>
+    <title>Choroby wirusowe w stomatologii</title>
 </head>
 
 <body>
     wrr brr 
     <?php
-    echo "xxxxxx";
+    $dbconn = pg_connect("host=localhost dbname=wirusy user=moj_uzytkownik password=moje_haslo");
+    $query = "SELECT * FROM wirusy";
+    $result = pg_query($dbconn, $query);
+    
+    echo "<table>";
+    while ($row = pg_fetch_assoc($result)) {
+        echo "<tr>";
+        echo "<td>" . $row['ID'] . "</td>";
+        echo "<td>" . $row['Jednostka chorobowa'] . "</td>";
+        echo "<td>" . $row['Czynnik etiologiczny'] . "</td>";
+        echo "<td>" . $row['Objawy ogólne i miejscowe poza jamą ustną'] . "</td>";
+        echo "<td>" . $row['Objawy miejscowe w jamie ustnej'] . "</td>";
+        echo "<td>" . $row['Rozpoznanie'] . "</td>";
+        echo "<td>" . $row['Różnicowanie'] . "</td>";
+        echo "</tr>";
+    }
+    echo "</table>";
+    
+    pg_close($dbconn);
     ?>
 </body>
 
