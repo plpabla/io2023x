c7df6f9e5f189a17dc735d4d3f0e6a97d07aa779
pawel-laptop <p.blaszczy@gmail.com>
2023-05-30 21:39:35 +0200

    wyświetlanie w tabeli

--- index.php
+++ index.php
@@ -41,7 +41,7 @@
 
       pg_close($conn);
     }
-    
+
     ?>
 
     <a href="dodaj_chorobe.php">Link do dodania nowej choroby</a>
@@ -50,7 +50,7 @@
     <thead>
       <tr>
         <th>ID</th>
-        <th>ID Wirus</th>
+        <th>Wirus.nazwa - to bedzie link</th>
         <th>Choroba</th>
         <th>Objawy Ogólne</th>
         <th>Objawy JU</th>
@@ -62,29 +62,35 @@
     </thead>
     <tbody>
       <?php
-      /*
+      
         // Tu wykonujesz połączenie z bazą danych PostgreSQL i wykonujesz zapytanie do tabeli "choroba" aby pobrać dane
         // Przykładowy kod PHP dla takiego połączenia i zapytania:
-        $conn = pg_connect("host=adres_hosta dbname=nazwa_bazy_danych user=uzytkownik password=haslo");
+        $conn = pg_connect(get_conn_string());
         $query = "SELECT id, id_wirus, choroba, objawy_ogolne, objawy_ju, rozpoznanie, roznicowanie FROM choroba";
         $result = pg_query($conn, $query);
-        
+       
         // Iterujesz przez wyniki zapytania i generujesz wiersze tabeli HTML
         while ($row = pg_fetch_assoc($result)) {
+          $id_wirus = $row['id_wirus'];
+          $query2 = "SELECT nazwa FROM wirus WHERE id=${id_wirus}";
+          $res_wir = pg_fetch_row(pg_query($conn, $query2));
+
           echo "<tr>";
           echo "<td>" . $row['id'] . "</td>";
-          echo "<td>" . $row['id_wirus'] . "</td>";
+          echo "<td>" . $res_wir[0] . "</td>";
           echo "<td>" . $row['choroba'] . "</td>";
           echo "<td>" . $row['objawy_ogolne'] . "</td>";
           echo "<td>" . $row['objawy_ju'] . "</td>";
           echo "<td>" . $row['rozpoznanie'] . "</td>";
           echo "<td>" . $row['roznicowanie'] . "</td>";
+          echo "<td> link </td>";
+          echo "<td> link </td>";
           echo "</tr>";
         }
         
         // Zamykasz połączenie
         pg_close($conn);
-      */
+      
       ?>
     </tbody>
   </table>
