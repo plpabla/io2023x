585ccab18b270e127635e28e0854083a9a175407
puellamiranda <kordekewa@gmail.com>
2023-05-31 20:08:51 +0200

    wycofanie zmian

--- dodaj_chorobe.php
+++ dodaj_chorobe.php
@@ -7,42 +7,6 @@
 </head>
 
 <body>
-<!--
-<?php
-// Dane do połączenia z bazą danych
-$host = "dbhost";
-$dbname = "dbname";
-$username = "dbuser";
-$password = "dbpass";
-
-// Tworzenie połączenia z bazą danych
-$pdo = new PDO("pgsql:host=$host;dbname=$dbname", $username, $password);
-
-// Pobieranie danych dla comboboxa
-$query = "SELECT id, nazwa FROM wirus";
-$statement = $pdo->prepare($query);
-$statement->execute();
-$rows = $statement->fetchAll(PDO::FETCH_ASSOC);
-?>
--->
-<?php
-    function get_conn_string()
-    {
-      $ini = parse_ini_file("php.ini");
-      $host = $ini["dbhost"];
-      $db = $ini["dbname"];
-      $usr = $ini["dbuser"];
-      $pass = $ini["dbpass"];
-      $conn_string = "host=$host port=5432 dbname=$db user=$usr password=$pass";
-      return $conn_string;
-    }
-    ?>
-// 
-    <select>
-        <?php foreach ($rows as $row): ?>
-            <option value="<?php echo $row['id']; ?>"><?php echo $row['nazwa']; ?></option>
-        <?php endforeach; ?>
-    </select>
     <form action="adres_do_przetwarzania_danych.php" method="post">
         <label for="jednostka_chorobowa">Jednostka chorobowa:</label>
         <input type="text" id="jednostka_chorobowa" name="jednostka_chorobowa" required><br>
