84b4ee901328938b31f4ad744fc46acbbb20b8ce
puellamiranda <kordekewa@gmail.com>
2023-05-31 21:27:09 +0200

    combobox 16 albo 17

--- dodaj_chorobe.php
+++ dodaj_chorobe.php
@@ -22,39 +22,39 @@
     $conn = pg_connect(get_conn_string());
     $query = "SELECT nazwa FROM wirusy";
     $result = pg_query($conn, $query);
-
-    echo '<form action="adres_do_przetwarzania_danych.php" method="post">';
-    echo '<label for="wirus">Wirus:</label>';
-    echo '<select id="wirus" name="wirus">';
-    while ($row = pg_fetch_assoc($result)) {
-        echo '<option value="' . $row['nazwa'] . '">' . $row['nazwa'] . '</option>';
-    }
-    echo '</select>';
-
-    pg_close($conn);
     ?>
 
-    <br>
+    <form action="adres_do_przetwarzania_danych.php" method="post">
+        <label for="wirus">Wirus:</label>
+        <select id="wirus" name="wirus">
+            <?php
+            while ($row = pg_fetch_assoc($result)) {
+                echo '<option value="' . $row['nazwa'] . '">' . $row['nazwa'] . '</option>';
+            }
+            pg_close($conn);
+            ?>
+        </select>
 
-    <label for="jednostka_chorobowa">Jednostka chorobowa:</label>
-    <input type="text" id="jednostka_chorobowa" name="jednostka_chorobowa" required><br>
+        <br>
 
-    <label for="objawy_ogolne_miejscowe">Objawy ogólne i miejscowe poza j.u.:</label>
-    <textarea id="objawy_ogolne_miejscowe" name="objawy_ogolne_miejscowe" required></textarea><br>
+        <label for="jednostka_chorobowa">Jednostka chorobowa:</label>
+        <input type="text" id="jednostka_chorobowa" name="jednostka_chorobowa" required><br>
 
-    <label for="objawy_miejscowe_ju">Objawy miejscowe w j.u.:</label>
-    <textarea id="objawy_miejscowe_ju" name="objawy_miejscowe_ju" required></textarea><br>
+        <label for="objawy_ogolne_miejscowe">Objawy ogólne i miejscowe poza j.u.:</label>
+        <textarea id="objawy_ogolne_miejscowe" name="objawy_ogolne_miejscowe" required></textarea><br>
 
-    <label for="rozpoznanie">Rozpoznanie:</label>
-    <textarea id="rozpoznanie" name="rozpoznanie" required></textarea><br>
+        <label for="objawy_miejscowe_ju">Objawy miejscowe w j.u.:</label>
+        <textarea id="objawy_miejscowe_ju" name="objawy_miejscowe_ju" required></textarea><br>
 
-    <label for="roznicowanie">Różnicowanie:</label>
-    <textarea id="roznicowanie" name="roznicowanie" required></textarea><br>
+        <label for="rozpoznanie">Rozpoznanie:</label>
+        <textarea id="rozpoznanie" name="rozpoznanie" required></textarea><br>
 
-    <input type="submit" value="Wyślij">
+        <label for="roznicowanie">Różnicowanie:</label>
+        <textarea id="roznicowanie" name="roznicowanie" required></textarea><br>
+
+        <input type="submit" value="Wyślij">
     </form>
 
 </body>
 
 </html>
-
