e12deb2691495d8a545c95c78d4bfcaf9cadfd2b
puellamiranda <kordekewa@gmail.com>
2023-05-31 21:48:25 +0200

    combobox 20

--- dodaj_chorobe.php
+++ dodaj_chorobe.php
@@ -18,27 +18,31 @@
       $conn_string = "host=$host port=5432 dbname=$db user=$usr password=$pass";
       return $conn_string;
     }
-    ?>
+?>
+
+    <form action="adres_do_przetwarzania_danych.php" method="post">
         <label for="wirus">Wirus:</label>
         <select id="wirus" name="wirus">
-        <?php
+            <?php
             $conn = pg_connect(get_conn_string());
             $query = "SELECT id_wirus, choroba, objawy_ogolne, objawy_ju, rozpoznanie, roznicowanie FROM choroba";
             $result = pg_query($conn, $query);
-            
+
             while ($row = pg_fetch_assoc($result)) {
                 $id_wirus = $row['id_wirus'];
-                $query2 = "SELECT nazwa FROM wirus WHERE id=${id_wirus}";
-                $res_wir = pg_fetch_row(pg_query($conn, $query2));
-                
-                echo '<option value="' . $row['nazwa'] . '">' . $row['nazwa'] . '</option>';
+                $query2 = "SELECT nazwa FROM wirus WHERE id_wirus=$id_wirus";
+                $res_wir = pg_query($conn, $query2);
+                $wirus = pg_fetch_assoc($res_wir);
+
+                echo '<option value="' . $wirus['nazwa'] . '">' . $wirus['nazwa'] . '</option>';
             }
 
             pg_close($conn);
             ?>
         </select>
 
-    <form action="adres_do_przetwarzania_danych.php" method="post">
+        <br>
+
         <label for="jednostka_chorobowa">Jednostka chorobowa:</label>
         <input type="text" id="jednostka_chorobowa" name="jednostka_chorobowa" required><br>
 
