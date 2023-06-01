6eb365cfbafc60940422b64db4736adc141f1bfa
puellamiranda <kordekewa@gmail.com>
2023-06-02 01:29:43 +0200

    korekta nazw w bazie danych

--- doc/BDwirusy.sql
+++ doc/BDwirusy.sql
@@ -1,13 +1,14 @@
 CREATE TABLE choroba (
   id SERIAL PRIMARY KEY,
   id_wirus INTEGER,
-  choroba VARCHAR,
-  objawy_ogolne VARCHAR,
-  objawy_ju VARCHAR,
+  jednostka_chorobowa VARCHAR,
+  objawy_ogolne_miejscowe VARCHAR,
+  objawy_miejscowe_ju VARCHAR,
   rozpoznanie VARCHAR,
   roznicowanie VARCHAR,
   FOREIGN KEY (id_wirus) REFERENCES wirus (id)
 );
+
 
 CREATE TABLE wirus (
   id SERIAL PRIMARY KEY,
