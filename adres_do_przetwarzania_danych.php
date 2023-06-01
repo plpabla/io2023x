c7a10fd7f92277e0adfc632f6f9f75954b6e7035
puellamiranda <kordekewa@gmail.com>
2023-06-02 00:20:53 +0200

    tu miało być dodanie choroby do tabeli

--- adres_do_przetwarzania_danych.php
+++ adres_do_przetwarzania_danych.php
@@ -1,8 +1,5 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Połączenie z bazą danych PostgreSQL
-    $dbconn = pg_connect("host=$host dbname=$db user=$usr password=$pass");
-
     // Pobranie danych z formularza
     $jednostka_chorobowa = $_POST['jednostka_chorobowa'];
     $objawy_ogolne_miejscowe = $_POST['objawy_ogolne_miejscowe'];
@@ -10,30 +7,44 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $rozpoznanie = $_POST['rozpoznanie'];
     $roznicowanie = $_POST['roznicowanie'];
 
-    // Zabezpieczenie przed SQL Injection
-    $jednostka_chorobowa = pg_escape_string($jednostka_chorobowa);
-    $objawy_ogolne_miejscowe = pg_escape_string($objawy_ogolne_miejscowe);
-    $objawy_miejscowe_ju = pg_escape_string($objawy_miejscowe_ju);
-    $rozpoznanie = pg_escape_string($rozpoznanie);
-    $roznicowanie = pg_escape_string($roznicowanie);
-
-    // Wykonanie zapytania INSERT do bazy danych
-    $query = "INSERT INTO choroba (jednostka_chorobowa, objawy_ogolne_miejscowe, objawy_miejscowe_ju, rozpoznanie, roznicowanie) 
-              VALUES ('$jednostka_chorobowa', '$objawy_ogolne_miejscowe', '$objawy_miejscowe_ju', '$rozpoznanie', '$roznicowanie')";
-    $result = pg_query($dbconn, $query);
-
-    // Sprawdzenie wyniku zapytania
-    if ($result) {
-        // Przekierowanie użytkownika na stronę index.php po pomyślnym dodaniu rekordu
-        pg_close($dbconn);
-        header("Location: index.php?message=Baza+wirus%C3%B3w+zosta%C5%82a+zaktualizowana");
-        exit();
+    // Sprawdzanie czy pola formularza nie są puste
+    if (empty($jednostka_chorobowa) || empty($objawy_ogolne_miejscowe) || empty($objawy_miejscowe_ju) || empty($rozpoznanie) || empty($roznicowanie)) {
+        echo "Wypełnij wszystkie pola formularza.";
     } else {
-        echo "Wystąpił błąd podczas dodawania rekordu.";
-    }
+        // Połączenie z bazą danych PostgreSQL
+        function get_conn_string()
+        {
+            $ini = parse_ini_file("php.ini");
+            $host = $ini["dbhost"];
+            $db = $ini["dbname"];
+            $usr = $ini["dbuser"];
+            $pass = $ini["dbpass"];
+            $conn_string = "host=$host port=5432 dbname=$db user=$usr password=$pass";
+            return $conn_string;
+        }
 
-    // Zamknięcie połączenia z bazą danych
-    pg_close($dbconn);
+        $conn = pg_connect(get_conn_string());
+
+        // Zabezpieczenie przed SQL Injection
+        $jednostka_chorobowa = pg_escape_string($jednostka_chorobowa);
+        $objawy_ogolne_miejscowe = pg_escape_string($objawy_ogolne_miejscowe);
+        $objawy_miejscowe_ju = pg_escape_string($objawy_miejscowe_ju);
+        $rozpoznanie = pg_escape_string($rozpoznanie);
+        $roznicowanie = pg_escape_string($roznicowanie);
+
+        // Wstawienie danych choroby do bazy danych
+        $query = "INSERT INTO choroba (jednostka_chorobowa, objawy_ogolne_miejscowe, objawy_miejscowe_ju, rozpoznanie, roznicowanie) 
+                  VALUES ('$jednostka_chorobowa', '$objawy_ogolne_miejscowe', '$objawy_miejscowe_ju', '$rozpoznanie', '$roznicowanie')";
+        $result = pg_query($conn, $query);
+
+        if ($result) {
+            echo "Choroba została dodana do bazy danych.";
+        } else {
+            echo "Wystąpił błąd podczas dodawania choroby do bazy danych.";
+        }
+
+        // Zamknięcie połączenia z bazą danych
+        pg_close($conn);
+    }
 }
 ?>
-
