4cb6528add847f71e82f36933f31a4a53ec5fa91
puellamiranda <kordekewa@gmail.com>
2023-06-01 23:58:51 +0200

    Powiązanie comboboxa z nazwą  wirusa

--- dodaj_chorobe.php
+++ dodaj_chorobe.php
@@ -1,13 +1,14 @@
 <!DOCTYPE html>
-<html lang="en">
-
+<html>
 <head>
-    <meta charset="utf-8">
-    <title>Dodawanie jednostki chorobowej</title>
+  <title>Dodaj Chorobę</title>
 </head>
 
 <body>
-<?php
+  <h1>Dodaj Nową Chorobę</h1>
+
+  <?php
+  if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     function get_conn_string()
     {
       $ini = parse_ini_file("php.ini");
@@ -18,76 +19,60 @@
       $conn_string = "host=$host port=5432 dbname=$db user=$usr password=$pass";
       return $conn_string;
     }
-    ?>
-        
-        <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Pobierz wartości wprowadzone przez użytkownika
-    $jednostka_chorobowa = $_POST["jednostka_chorobowa"];
-    $objawy_ogolne_miejscowe = $_POST["objawy_ogolne_miejscowe"];
-    $objawy_miejscowe_ju = $_POST["objawy_miejscowe_ju"];
-    $rozpoznanie = $_POST["rozpoznanie"];
-    $roznicowanie = $_POST["roznicowanie"];
 
-    // Połączenie z bazą danych
+    // Połączenie z bazą danych PostgreSQL
     $conn = pg_connect(get_conn_string());
 
-    // Przygotowanie i wykonanie zapytania SQL
-    $query = "INSERT INTO tabela (jednostka_chorobowa, objawy_ogolne_miejscowe, objawy_miejscowe_ju, rozpoznanie, roznicowanie)
-              VALUES ('$jednostka_chorobowa', '$objawy_ogolne_miejscowe', '$objawy_miejscowe_ju', '$rozpoznanie', '$roznicowanie')";
+    // Pobranie danych z formularza
+    $jednostka_chorobowa = $_POST['jednostka_chorobowa'];
+    $id_wirus = $_POST['id_wirus'];
+    $objawy_ogolne_miejscowe = $_POST['objawy_ogolne_miejscowe'];
+    $objawy_miejscowe_ju = $_POST['objawy_miejscowe_ju'];
+    $rozpoznanie = $_POST['rozpoznanie'];
+    $roznicowanie = $_POST['roznicowanie'];
+
+    // Wstawienie nowego rekordu do tabeli choroba
+    $query = "INSERT INTO choroba (jednostka_chorobowa, id_wirus, objawy_ogolne_miejscowe, objawy_miejscowe_ju, rozpoznanie, roznicowanie)
+              VALUES ('$jednostka_chorobowa', $id_wirus, '$objawy_ogolne_miejscowe', '$objawy_miejscowe_ju', '$rozpoznanie', '$roznicowanie')";
     $result = pg_query($conn, $query);
 
-    // Sprawdzenie, czy zapytanie zostało wykonane poprawnie
     if ($result) {
-        echo "Dane zostały zapisane do bazy danych.";
+      echo "Dodano nową chorobę.";
     } else {
-        echo "Wystąpił błąd podczas zapisywania danych.";
+      echo "Wystąpił błąd podczas dodawania choroby.";
     }
 
     // Zamknięcie połączenia z bazą danych
     pg_close($conn);
-}
-?>
+  }
+  ?>
 
-    <form action="adres_do_przetwarzania_danych.php" method="post">
+  <form method="POST">
+    <label for="jednostka_chorobowa">Jednostka chorobowa:</label><br>
+    <input type="text" id="jednostka_chorobowa" name="jednostka_chorobowa" required><br><br>
 
-    <label for="wirus" style="width: 250px; height: 30px;">Wirus lub rodzina wirusów:</label>
-        <select id="wirus" name="wirus" style="width: 250px; height: 30px;">
-        <?php
-            $conn = pg_connect(get_conn_string());
-            $query = "SELECT id_wirus, choroba, objawy_ogolne, objawy_ju, rozpoznanie, roznicowanie FROM choroba";
-            $result = pg_query($conn, $query);
-            
-            while ($row = pg_fetch_assoc($result)) {
-                $id_wirus = $row['id_wirus'];
-                $query2 = "SELECT nazwa FROM wirus WHERE id=${id_wirus}";
-                $res_wir = pg_fetch_row(pg_query($conn, $query2));
-                
-                echo '<option value="' . $row['nazwa'] . '">' . $row['nazwa'] . '</option>';
-            }
+    <label for="id_wirus">ID wirusa:</label><br>
+    <select id="id_wirus" name="id_wirus" required>
+      <option value="">Wybierz wirusa</option>
+      <option value="1">Wirus A</option>
+      <option value="2">Wirus B</option>
+      <option value="3">Wirus C</option>
+      <!-- Dodaj tutaj pozostałe opcje dla innych wirusów -->
+    </select><br><br>
 
-            pg_close($conn);
-            ?>
-        </select><br>
+    <label for="objawy_ogolne_miejscowe">Objawy ogólne lub miejscowe poza jamą ustną:</label><br>
+    <textarea id="objawy_ogolne_miejscowe" name="objawy_ogolne_miejscowe" required></textarea><br><br>
 
-        <label for="jednostka_chorobowa" style="width: 250px; height: 30px;">Jednostka chorobowa:</label>
-        <input type="text" id="jednostka_chorobowa" name="jednostka_chorobowa" style="width: 250px; height: 30px" required><br>
+    <label for="objawy_miejscowe_ju">Objawy miejscowe w jamie ustnej:</label><br>
+    <textarea id="objawy_miejscowe_ju" name="objawy_miejscowe_ju" required></textarea><br><br>
 
-        <label for="objawy_ogolne_miejscowe" style="width: 300px; height: 30px;">Objawy ogólne i miejscowe poza j.u.:</label><br>
-        <textarea id="objawy_ogolne_miejscowe" name="objawy_ogolne_miejscowe" style="width: 300px; height: 150px;" required></textarea><br>
+    <label for="rozpoznanie">Rozpoznanie:</label><br>
+    <textarea id="rozpoznanie" name="rozpoznanie" required></textarea><br><br>
 
-        <label for="objawy_miejscowe_ju" style="width: 300px; height: 30px;">Objawy miejscowe w j.u.:</label><br>
-        <textarea id="objawy_miejscowe_ju" name="objawy_miejscowe_ju" style="width: 300px; height: 150px;" required></textarea><br>
+    <label for="roznicowanie">Roznicowanie:</label><br>
+    <textarea id="roznicowanie" name="roznicowanie" required></textarea><br><br>
 
-        <label for="rozpoznanie" style="width: 300px; height: 30px;">Rozpoznanie:</label><br>
-        <textarea id="rozpoznanie" name="rozpoznanie" style="width: 300px; height: 80px;" required></textarea><br>
-
-        <label for="roznicowanie" style="width: 300px; height: 30px;">Różnicowanie:</label><br>
-        <textarea id="roznicowanie" name="roznicowanie" style="width: 300px; height: 80px;" required></textarea><br>
-
-        <input type="submit" value="Wyślij">
-    </form>
-
+    <input type="submit" value="Dodaj">
+  </form>
 </body>
-
 </html>
