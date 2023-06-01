dcdbbab1e2cdb889598aa82af76c31906932dd5e
puellamiranda <kordekewa@gmail.com>
2023-06-01 23:32:23 +0200

    dodanie linkow do edycji usuniecia, zmiana tabeli

--- index.php
+++ index.php
@@ -32,77 +32,66 @@
 
     <a href="dodaj_chorobe.php">Link do dodania nowej choroby</a>
     <br><br>
-  <table>
-    <thead>
-      <tr>
-        <th>ID</th>
-        <th>Jednostka chorobowa</th>
-        <th>Czynnik etiologiczny - to bedzie link</th>
-        <th>Objawy ogólne lub miejscowe poza jamą ustną</th>
-        <th>Objawy miejscowe w jamie ustnej</th>
-        <th>Rozpoznanie</th>
-        <th>Różnicowanie</th>
-        <th>Edytuj</th>
-        <th>Usuń</th>
-      </tr>
-    </thead>
-    <tbody>
-      <?php
+  <?php
       
-        // Tu wykonujesz połączenie z bazą danych PostgreSQL i wykonujesz zapytanie do tabeli "choroba" aby pobrać dane
-        // Przykładowy kod PHP dla takiego połączenia i zapytania:
-        $conn = pg_connect(get_conn_string());
-        $query = "SELECT id, id_wirus, choroba, objawy_ogolne, objawy_ju, rozpoznanie, roznicowanie FROM choroba";
-        $result = pg_query($conn, $query);
-       
-         // Sprawdzanie, czy są dostępne dane
+    // Tu wykonujesz połączenie z bazą danych PostgreSQL i wykonujesz zapytanie do tabeli "choroba" aby pobrać dane
+    // Przykładowy kod PHP dla takiego połączenia i zapytania:
+    $conn = pg_connect(get_conn_string());
+    $query = "SELECT id, id_wirus, choroba, objawy_ogolne, objawy_ju, rozpoznanie, roznicowanie FROM choroba";
+    $result = pg_query($conn, $query);
+
+    // Sprawdzanie, czy są dostępne dane
     if (pg_num_rows($result) > 0) {
       echo '<table>
-              <tr>
+              <thead>
+                <tr>
+                  <th>ID</th>
                   <th>Jednostka chorobowa</th>
-                  <th>Objawy ogólne i miejscowe poza j.u.</th>
-                  <th>Objawy miejscowe w j.u.</th>
+                  <th>Czynnik etiologiczny - to bedzie link</th>
+                  <th>Objawy ogólne lub miejscowe poza jamą ustną</th>
+                  <th>Objawy miejscowe w jamie ustnej</th>
                   <th>Rozpoznanie</th>
                   <th>Różnicowanie</th>
-              </tr>';
+                  <th>Edytuj</th>
+                  <th>Usuń</th>
+                </tr>
+              </thead>
+              <tbody>';
 
-        // Iterujesz przez wyniki zapytania i generujesz wiersze tabeli HTML
-        while ($row = pg_fetch_assoc($result)) {
-          // Dla każdego wiersza pobierz też dane wirusa, które chcesz wyświetlić
-          $id_wirus = $row['id_wirus'];
-          $query2 = "SELECT nazwa FROM wirus WHERE id=${id_wirus}";
-          $res_wir = pg_fetch_row(pg_query($conn, $query2));
+      // Iterujesz przez wyniki zapytania i generujesz wiersze tabeli HTML
+      while ($row = pg_fetch_assoc($result)) {
+        // Dla każdego wiersza pobierz też dane wirusa, które chcesz wyświetlić
+        $id_wirus = $row['id_wirus'];
+        $query2 = "SELECT nazwa FROM wirus WHERE id={$id_wirus}";
+        $res_wir = pg_fetch_row(pg_query($conn, $query2));
 
-          echo "<tr>";
-          echo "<td>" . $row['id'] . "</td>";
-          echo "<td>" . $row['choroba'] . "</td>";
-          echo "<td>" . $res_wir[0] . "</td>";
-          echo "<td>" . $row['objawy_ogolne'] . "</td>";
-          echo "<td>" . $row['objawy_ju'] . "</td>";
-          echo "<td>" . $row['rozpoznanie'] . "</td>";
-          echo "<td>" . $row['roznicowanie'] . "</td>";
-          echo "<td> link </td>";
-          echo "<td> link </td>";
-          echo "</tr>";
-        }
-        
-        echo '</table>';
+        echo "<tr>";
+        echo "<td>" . $row['id'] . "</td>";
+        echo "<td>" . $row['choroba'] . "</td>";
+        echo "<td>" . $res_wir[0] . "</td>";
+        echo "<td>" . $row['objawy_ogolne'] . "</td>";
+        echo "<td>" . $row['objawy_ju'] . "</td>";
+        echo "<td>" . $row['rozpoznanie'] . "</td>";
+        echo "<td>" . $row['roznicowanie'] . "</td>";
+        echo "<td><a href='edytuj_chorobe.php?id={$row['id']}'>Edytuj</a></td>";
+        echo "<td><a href='usun_chorobe.php?id={$row['id']}'>Usuń</a></td>";
+        echo "</tr>";
+      }
+
+      echo '</tbody></table>';
     } else {
-        echo 'Brak danych do wyświetlenia.';
+      echo 'Brak danych do wyświetlenia.';
     }
-        // Zamykasz połączenie
-        pg_close($conn);
-      
-      ?>
 
-        <?php
-if (isset($_GET['message'])) {
-    $message = $_GET['message'];
-    echo "<script>alert('" . $message . "');</script>";
-}
-?>
+    // Zamykasz połączenie
+    pg_close($conn);
+  ?>
 
-    </tbody>
-  </table>
+  <?php
+  if (isset($_GET['message'])) {
+      $message = $_GET['message'];
+      echo "<script>alert('" . $message . "');</script>";
+  }
+  ?>
 </body>
 </html>
