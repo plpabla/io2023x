ae3f1b051e029dc29ecce816c721bf0161e5f24b
pawel-laptop <p.blaszczy@gmail.com>
2023-06-03 10:29:59 +0200

    wincyj info

--- wirusy.php
+++ wirusy.php
@@ -28,12 +28,16 @@
     $selected_id = 4;
     if (isset($_GET["nazwa"]))
     {
-        $selected_str = urlencode($_GET["nazwa"]);
+        $selected_str = urldecode($_GET["nazwa"]);
         echo "poszukiwanie ${selected_str}<br>";
         $selected_str = pg_escape_string($conn, $selected_str);
         echo "poszukiwanie ${selected_str}<br>";
         $query = "SELECT id FROM wirus WHERE nazwa=${selected_str}";
         $result = pg_query($conn, $query);
+        if($result)
+            echo "cos jest";
+            else
+            echo "nie ma";
         $row = pg_fetch_assoc($result);
         print_r($row);
         $selected_id = $row['id'];
