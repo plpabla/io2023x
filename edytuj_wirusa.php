e1c8717098eb16047c74bc49882a1c6b9bc602e7
pawel-laptop <p.blaszczy@gmail.com>
2023-06-04 09:45:19 +0200

    poprawa wyglądu formularza edycji wirusa

--- edytuj_wirusa.php
+++ edytuj_wirusa.php
@@ -57,39 +57,54 @@ pg_close($conn);
 <html>
 <head>
     <title>Edytuj Wirusa</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 </head>
 <body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+
     <h2>Edytuj Wirusa</h2>
 
     <form action="edytuj_wirusa.php?id=<?php echo $id; ?>" method="POST">
-        <label for="nazwa">Nazwa wirusa:</label>
-        <input type="text" id="nazwa" name="nazwa" value="<?php echo $row['nazwa']; ?>" required>
-        <br><br>
+        <div class="form-floating mb-3">    
+            <input class="form-control" type="text" id="nazwa" name="nazwa" value="<?php echo $row['nazwa']; ?>" required>
+            <label class="form-label" for="nazwa">Nazwa wirusa:</label>
+        </div>
+    
+        <div class="form-floating mb-3">  
+            <input class="form-control" type="text" id="skrot" name="skrot" value="<?php echo $row['skrot']; ?>" required>
+            <label class="form-label" for="skrot">Skrót:</label>
+        </div>
 
-        <label for="skrot">Skrót:</label>
-        <input type="text" id="skrot" name="skrot" value="<?php echo $row['skrot']; ?>" required>
-        <br><br>
+        <div class="form-floating mb-3"> 
+            <input class="form-control" type="text" id="genom" name="genom" value="<?php echo $row['genom']; ?>" required>
+            <label class="form-label" for="genom">Genom:</label>
+        </div>
 
-        <label for="genom">Genom:</label>
-        <input type="text" id="genom" name="genom" value="<?php echo $row['genom']; ?>" required>
-        <br><br>
+        <div class="form-floating mb-3"> 
+            <input class="form-control" type="text" id="wyleganie" name="wyleganie" value="<?php echo $row['wyleganie']; ?>" required>
+            <label class="form-label" for="wyleganie">Wyleganie:</label>
+        </div>
 
-        <label for="wyleganie">Wyleganie:</label>
-        <input type="text" id="wyleganie" name="wyleganie" value="<?php echo $row['wyleganie']; ?>" required>
-        <br><br>
+        <div class="form-floating mb-3"> 
+            <input class="form-control" type="text" id="szczepionka" name="szczepionka" value="<?php echo $row['szczepionka']; ?>" required>
+            <label class="form-label" for="szczepionka">Szczepionka:</label>
+        </div>
 
-        <label for="szczepionka">Szczepionka:</label>
-        <input type="text" id="szczepionka" name="szczepionka" value="<?php echo $row['szczepionka']; ?>" required>
-        <br><br>
+        <div class="form-floating mb-3">
+            <input class="form-control" type="text" id="droga_zak" name="droga_zak" value="<?php echo $row['droga_zak']; ?>" required>
+            <label class="form-label" for="droga_zak">Droga zakażenia:</label>
+        </div>
 
-        <label for="droga_zak">Droga zakażenia:</label>
-        <input type="text" id="droga_zak" name="droga_zak" value="<?php echo $row['droga_zak']; ?>" required>
-        <br><br>
-
-        <input type="submit" name="submit" value="Zapisz zmiany">
+        <div class="col-12">
+            <button class="btn btn-primary" type="submit">Zapisz zmiany</button>
+        </div>
     </form>
+
+    <br>
+    <button type="button" class="btn btn-secondary" onclick="location.href='wirusy.php';">Powrót do charakterystyki wirusów</button>
+    <br><br>
+    <button type="button" class="btn btn-secondary" onclick="location.href='index.php';">Wróć na stronę główną</button>
 </body>
 </html>
 
 <br><br>
-<a href="wirusy.php">Powrót do charakterystyki wirusów</a>
