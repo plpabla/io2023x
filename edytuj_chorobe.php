fefe9f4da367a36002f52c028b0eedff595d779f
puellamiranda <kordekewa@gmail.com>
2023-06-04 11:00:21 +0200

    powrót do źródła, nauka cierpliwości

--- edytuj_chorobe.php
+++ edytuj_chorobe.php
@@ -1,3 +1,117 @@
+<?php
+function get_conn_string()
+{
+    $ini = parse_ini_file("php.ini");
+    $host = $ini["dbhost"];
+    $db = $ini["dbname"];
+    $usr = $ini["dbuser"];
+    $pass = $ini["dbpass"];
+    $conn_string = "host=$host port=5432 dbname=$db user=$usr password=$pass";
+    return $conn_string;
+}
+
+// Pobierz identyfikator choroby z parametru URL
+$id = $_GET['id'];
+
+// Obsługa aktualizacji
+if (isset($_POST['submit'])) {
+    // Pobierz wartości z formularza i zabezpiecz przed SQL Injection
+    $choroba = pg_escape_string($_POST['choroba']);
+    $objawy_ogolne = pg_escape_string($_POST['objawy_ogolne']);
+    $objawy_ju = pg_escape_string($_POST['objawy_ju']);
+    $rozpoznanie = pg_escape_string($_POST['rozpoznanie']);
+    $roznicowanie = pg_escape_string($_POST['roznicowanie']);
+    $id_wirus = $_POST['id_wirus'];
+
+    // Połączenie z bazą danych PostgreSQL
+    $conn = pg_connect(get_conn_string());
+    
+    // Aktualizacja danych choroby w bazie danych i zabezpieczenie przed SQL Injection
+    $query = "UPDATE choroba SET choroba = '$choroba', objawy_ogolne = '$objawy_ogolne', objawy_ju = '$objawy_ju', rozpoznanie = '$rozpoznanie', roznicowanie = '$roznicowanie', id_wirus = $id_wirus WHERE id = $id";
+    pg_query($conn, $query);
+
+    // Zamknięcie połączenia z bazą danych
+    pg_close($conn);
+
+    // Wyświetlenie messageboxa po aktualizacji bazy wirusów
+    echo "<script>alert('Baza wirusów została zaktualizowana');</script>";
+}
+
+// Połączenie z bazą danych PostgreSQL
+$conn = pg_connect(get_conn_string());
+
+$query = "SELECT c.id, c.choroba, w.nazwa, c.objawy_ogolne, c.objawy_ju, c.rozpoznanie, c.roznicowanie, c.id_wirus
+          FROM choroba c
+          JOIN wirus w ON c.id_wirus = w.id
+          WHERE c.id = $id";
+$result = pg_query($conn, $query);
+$row = pg_fetch_assoc($result);
+
+// Zamknięcie połączenia z bazą danych
+pg_close($conn);
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Edytuj chorobę wirusową</title>
+</head>
+<body>
+    <h2>Edytuj chorobę wirusową</h2>
+
+    <form action="edytuj_chorobe.php?id=<?php echo $id; ?>" method="POST">
+        <label for="choroba">Jednostka chorobowa:</label>
+        <input type="text" id="choroba" name="choroba" value="<?php echo $row['choroba']; ?>" required>
+        <br><br>
+
+        <label for="id_wirus">Czynnik etiologiczny:</label>
+        <select id="id_wirus" name="id_wirus" required>
+            <?php
+            // Połączenie z bazą danych PostgreSQL
+            $conn = pg_connect(get_conn_string());
+
+            // Pobranie wszystkich wirusów i zabezpieczenie przed SQL Injection
+            $query = "SELECT * FROM wirus ORDER BY id";
+            $result = pg_query($conn, $query);
+
+            // Iterujesz przez wyniki zapytania i generujesz opcje w formularzu
+            while ($virus = pg_fetch_assoc($result)) {
+                $selected = ($virus['id'] == $row['id_wirus']) ? "selected" : "";
+                echo "<option value='{$virus['id']}' $selected>{$virus['nazwa']}</option>";
+            }
+
+            // Zamknięcie połączenia z bazą danych
+            pg_close($conn);
+            ?>
+        </select>
+        <br><br>
+
+        <label for="objawy_ogolne">Objawy ogólne lub miejscowe poza jamą ustną:</label>
+        <textarea id="objawy_ogolne" name="objawy_ogolne" required><?php echo $row['objawy_ogolne']; ?></textarea>
+        <br><br>
+
+        <label for="objawy_ju">Objawy miejscowe w jamie ustnej:</label>
+        <textarea id="objawy_ju" name="objawy_ju" required><?php echo $row['objawy_ju']; ?></textarea>
+        <br><br>
+
+        <label for="rozpoznanie">Rozpoznanie:</label>
+        <textarea id="rozpoznanie" name="rozpoznanie" required><?php echo $row['rozpoznanie']; ?></textarea>
+        <br><br>
+
+        <label for="roznicowanie">Różnicowanie:</label>
+        <textarea id="roznicowanie" name="roznicowanie" required><?php echo $row['roznicowanie']; ?></textarea>
+        <br><br>
+
+        <input type="submit" name="submit" value="Zapisz zmiany">
+    </form>
+</body>
+</html>
+
+<br><br>
+<a href="index.php">Powrót do strony głównej</a>
+
+
+<!--
 <!DOCTYPE html>
 <html>
 <head>
@@ -90,7 +204,7 @@
 
     <form action="edytuj_chorobe.php?id=<?php echo $id; ?>" method="POST">
     <div class="form-floating mb-3">
-            <!-- <span class="input-group-text" id="basic-addon1">Jednostka chorobowa</span> -->
+-->  <!-- <span class="input-group-text" id="basic-addon1">Jednostka chorobowa</span> --> <!--
             <input type="text" id="choroba" name="choroba" required class="form-control">
             <label for="choroba" class="form-label">Jednostka chorobowa:</label>
         </div>    
@@ -145,3 +259,4 @@
 
 </body>
 </html>
+            -->
