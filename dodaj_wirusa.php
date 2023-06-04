e2b137429c7c3ba15fccada37c33c091a6166516
pawel-laptop <p.blaszczy@gmail.com>
2023-06-04 10:02:58 +0200

    dodanie skryptów bootstrapa
    ale nic więcej :D

--- dodaj_wirusa.php
+++ dodaj_wirusa.php
@@ -3,8 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <title>Dodaj wirusa</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 </head>
 <body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
     <h1>Dodaj wirusa</h1>
 
     <?php
@@ -36,7 +38,8 @@
     pg_close($conn);
 
 
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if ('' === 'POST') {
         // Pobranie danych z formularza
         $nazwa = $_POST['nazwa'];
         $skrot = $_POST['skrot'];
