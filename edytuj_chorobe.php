b4e1b1836d7dbbf0a2c4304d1287573c98a7a184
puellamiranda <kordekewa@gmail.com>
2023-06-04 11:18:19 +0200

    miało być  ładniej

--- edytuj_chorobe.php
+++ edytuj_chorobe.php
@@ -56,33 +56,50 @@ pg_close($conn);
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Edytuj Chorobę</title>
+    <meta charset="UTF-8">
+    <title>Edytuj chorobę wirusową</title>
+
+    <script>
+        function showMessageBox() {
+            alert("Baza wirusów została zaktualizowana");
+        }
+    </script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+
 </head>
 <body>
-    <h2>Edytuj Chorobę</h2>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+    <h1>Edytuj chorobę wirusową</h1>
 
     <form action="edytuj_chorobe.php?id=<?php echo $id; ?>" method="POST">
-        <label for="choroba">Jednostka chorobowa:</label>
+    <div class="form-floating mb-3">
+            <!-- <span class="input-group-text" id="basic-addon1">Jednostka chorobowa</span> -->
+            <input type="text" id="choroba" name="choroba" required class="form-control">
+            <label for="choroba" class="form-label">Jednostka chorobowa:</label>
+        </div>
+ <!--       <label for="choroba">Jednostka chorobowa:</label>
         <input type="text" id="choroba" name="choroba" value="<?php echo $row['choroba']; ?>" required>
         <br><br>
+    -->
 
-        <label for="objawy_ogolne">Objawy ogólne lub miejscowe poza jamą ustną:</label>
-        <textarea id="objawy_ogolne" name="objawy_ogolne" required><?php echo $row['objawy_ogolne']; ?></textarea>
-        <br><br>
+    <label for="id_wirus">Wybierz wirusa:</label>  
+        <div class="row">
+            <div class="col">
+                <div class="form mb-3">
+                
+                <select class="form-select" id="id_wirus" name="id_wirus">
+                    <?php
+                    if (!empty($wirusy)) {
+                        foreach ($wirusy as $wirus) {
+                            echo "<option value='" . $wirus['id'] . "'>" . $wirus['nazwa'] . "</option>";
+                        }
+                    }
+                    ?>
+                </select>
+                </div>
+            </div>
 
-        <label for="objawy_ju">Objawy miejscowe w jamie ustnej:</label>
-        <textarea id="objawy_ju" name="objawy_ju" required><?php echo $row['objawy_ju']; ?></textarea>
-        <br><br>
-
-        <label for="rozpoznanie">Rozpoznanie:</label>
-        <textarea id="rozpoznanie" name="rozpoznanie" required><?php echo $row['rozpoznanie']; ?></textarea>
-        <br><br>
-
-        <label for="roznicowanie">Różnicowanie:</label>
-        <textarea id="roznicowanie" name="roznicowanie" required><?php echo $row['roznicowanie']; ?></textarea>
-        <br><br>
-
-        <label for="id_wirus">Czynnik etiologiczny (wirus):</label>
+<!--    <label for="id_wirus">Czynnik etiologiczny (wirus):</label>
         <select id="id_wirus" name="id_wirus" required>
             <?php
             // Połączenie z bazą danych PostgreSQL
@@ -103,8 +120,58 @@ pg_close($conn);
             ?>
         </select>
         <br><br>
+        -->   
+        
+        <div class="form-floating mb-3">
+            <input type="text" id="objawy_ogolne" name="objawy_ogolne" required class="form-control">
+            <label for="objawy_ogolne" class="form-label">Objawy ogólne/miejscowe:</label>
+        </div>
 
+        <div class="form-floating mb-3">
+            <input type="text" id="objawy_ju" name="objawy_ju" required class="form-control">
+            <label for="objawy_ju" class="form-label">Objawy miejscowe/ju:</label>
+        </div>
+
+        <div class="form-floating mb-3">
+            <input type="text" id="rozpoznanie" name="rozpoznanie" required class="form-control">
+            <label for="rozpoznanie" class="form-label">Rozpoznanie:</label>
+        </div>
+
+        <div class="form-floating mb-3">
+            <input type="text" id="roznicowanie" name="roznicowanie" required class="form-control">
+            <label for="roznicowanie" class="form-label">Różnicowanie:</label>
+        </div>
+
+        <div class="col-12">
+            <button class="btn btn-primary" type="submit">Dodaj chorobę</button>
+        </div>
+    </form>
+
+    <br><br>
+ <<!-- przycisk z dodawania choroby
+           <button type="button" class="btn btn-secondary" onclick="location.href='index.php';">Wróć na stronę główną</button>
+        -->
+
+    <!--
+        <label for="objawy_ogolne">Objawy ogólne lub miejscowe poza jamą ustną:</label>
+        <textarea id="objawy_ogolne" name="objawy_ogolne" required><?php echo $row['objawy_ogolne']; ?></textarea>
+        <br><br>
+
+        <label for="objawy_ju">Objawy miejscowe w jamie ustnej:</label>
+        <textarea id="objawy_ju" name="objawy_ju" required><?php echo $row['objawy_ju']; ?></textarea>
+        <br><br>
+
+        <label for="rozpoznanie">Rozpoznanie:</label>
+        <textarea id="rozpoznanie" name="rozpoznanie" required><?php echo $row['rozpoznanie']; ?></textarea>
+        <br><br>
+
+        <label for="roznicowanie">Różnicowanie:</label>
+        <textarea id="roznicowanie" name="roznicowanie" required><?php echo $row['roznicowanie']; ?></textarea>
+        <br><br>
+        -->
+        
         <input type="submit" name="submit" value="Zapisz zmiany">
+        
     </form>
 </body>
 </html>
