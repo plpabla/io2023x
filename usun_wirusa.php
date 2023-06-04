9c078930608f4f720bb76aab3289fedb7c235c5a
puellamiranda <kordekewa@gmail.com>
2023-06-04 14:49:16 +0200

    dodanie przycisków

--- usun_wirusa.php
+++ usun_wirusa.php
@@ -45,6 +45,12 @@
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+
+    <br><br>
+    <button type="button" class="btn btn-secondary" onclick="location.href='wirusy.php';">Wróć do listy wirusów</button>
+    <br><br>
+    <button type="button" class="btn btn-secondary" onclick="location.href='index.php';">Wróć na stronę główną</button>
+
 </body>
 </html>
 
