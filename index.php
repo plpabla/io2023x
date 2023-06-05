d771ee2b9eeb21699aed288a4256cb2a6bd1cf28
puellamiranda <kordekewa@gmail.com>
2023-06-06 00:06:31 +0200

    autouzupełnianie wersja 3

--- index.php
+++ index.php
@@ -10,24 +10,27 @@
         integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz"
         crossorigin="anonymous"></script>
 
-        <script>
-    $(document).ready(function () {
-        $('#search').typeahead({
-            source: function (query, result) {
-                $.ajax({
-                    url: "search.php",
-                    method: "GET", // Zmiana metody na GET
-                    data: { search: query }, // Przekazanie wartości 'search' jako parametr GET
-                    dataType: "json",
-                    success: function (data) {
-                        result($.map(data, function (item) {
-                            return item;
-                        }));
-                    }
-                });
-            }
+    <script>
+        $(document).ready(function () {
+            $('#search').typeahead({
+                source: function (query, result) {
+                    $.ajax({
+                        url: "search.php",
+                        method: "GET",
+                        data: { search: query },
+                        dataType: "json",
+                        success: function (data) {
+                            result($.map(data, function (item) {
+                                return item;
+                            }));
+                        }
+                    });
+                }
+            });
         });
-    });
+    </script>
+
+
 </script>
 
 </head>
@@ -63,7 +66,7 @@
         <form method="GET">
             <div class="form-group">
                 <label for="search">Wyszukaj:</label>
-                <input type="text" class="form-control" id="search" name="search" autocomplete="off">
+                <input type="text" class="form-control" id="search" name="search" autocomplete="nope">
             </div>
             <button type="submit" class="btn btn-primary">Szukaj</button>
         </form>
@@ -85,6 +88,87 @@
 
         <br>
   -->
+  <?php
+  // Połączenie z bazą danych PostgreSQL
+$conn = pg_connect(get_conn_string());
+
+// Pobranie wartości wyszukiwania z pola tekstowego
+$search = isset($_GET['search']) ? $_GET['search'] : '';
+
+// Zaktualizuj zapytanie SQL z warunkiem WHERE
+$query = "SELECT c.id, c.choroba, w.nazwa AS nazwa_wirusa, c.objawy_ogolne, c.objawy_ju, c.rozpoznanie, c.roznicowanie
+          FROM choroba c
+          JOIN wirus w ON c.id_wirus = w.id";
+
+if (!empty($search)) {
+    // Dodaj warunek WHERE, jeśli podano wartość wyszukiwania
+    $query .= " WHERE c.choroba ILIKE '%" . pg_escape_string($search) . "%'
+                OR w.nazwa ILIKE '%" . pg_escape_string($search) . "%'
+                OR c.objawy_ogolne ILIKE '%" . pg_escape_string($search) . "%'
+                OR c.objawy_ju ILIKE '%" . pg_escape_string($search) . "%'
+                OR c.rozpoznanie ILIKE '%" . pg_escape_string($search) . "%'
+                OR c.roznicowanie ILIKE '%" . pg_escape_string($search) . "%'";
+}
+
+$query .= " ORDER BY c.id";
+
+// Pobranie danych z tabeli choroba, wraz z nazwą wirusa
+$result = pg_query($conn, $query);
+
+// Sprawdzenie, czy przesłano wartość wyszukiwania
+if (!empty($search)) {
+    echo "<h3>Wyniki wyszukiwania dla: " . htmlentities($search) . "</h3>";
+}
+
+// Sprawdzenie, czy są dostępne dane
+if (pg_num_rows($result) > 0) {
+    echo '<table class="table table-striped">
+            <thead class="table-dark">
+                <tr>
+                    <th scope="col">Lp.</th>
+                    <th scope="col">Jednostka chorobowa</th>
+                    <th scope="col">Czynnik etiologiczny</th>
+                    <th scope="col">Objawy ogólne lub miejscowe poza jamą ustną</th>
+                    <th scope="col">Objawy miejscowe w jamie ustnej</th>
+                    <th scope="col">Rozpoznanie</th>
+                    <th scope="col">Różnicowanie</th>
+                    <th scope="col"></th>
+                    <th scope="col"></th>
+                </tr>
+            </thead>
+            <tbody>';
+
+    $lp = 1; // Zmienna licznikowa
+
+    // Iteracja przez wyniki zapytania i generowanie wierszy tabeli HTML
+    while ($row = pg_fetch_assoc($result)) {
+        echo "<tr>";
+        echo "<th scope='row'>" . $lp . "</td>";
+        echo "<td>" . $row['choroba'] . "</td>";
+        echo "<td><a href='wirusy.php?nazwa=" . urlencode($row['nazwa_wirusa']) . "'>" . $row['nazwa_wirusa'] . "</a></td>";
+        echo "<td>" . $row['objawy_ogolne'] . "</td>";
+        echo "<td>" . $row['objawy_ju'] . "</td>";
+        echo "<td>" . $row['rozpoznanie'] . "</td>";
+        echo "<td>" . $row['roznicowanie'] . "</td>";
+        echo "<td><button type='button' class='btn btn-primary' onclick='location.href=\"edytuj_chorobe.php?id={$row['id']}\"'>Edytuj</button></td>";
+        echo "<td><button type='button' class='btn btn-danger' onclick='location.href=\"usun_chorobe.php?id={$row['id']}\"'>Usuń</button></td>";
+        echo "</tr>";
+
+        $lp++; // Inkrementacja zmiennej licznikowej
+    }
+
+    echo '</tbody>
+        </table>';
+} else {
+    echo "Brak dostępnych danych.";
+}
+
+// Zamknięcie połączenia z bazą danych
+pg_close($conn);
+?>
+
+
+  <!--
     <?php
       
     // Połączenie z bazą danych PostgreSQL
@@ -169,6 +253,7 @@ $query .= " ORDER BY c.id";
     // Zamknięcie połączenia z bazą danych
     pg_close($conn);
   ?>
+  -->
 </div>
 
 </body>
