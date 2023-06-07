4e4a9c0b8015d07673a4c81dd13edd73ae830607
puellamiranda <kordekewa@gmail.com>
2023-06-07 15:15:29 +0200

    autouzupełnianie wersja 16

--- search.php
+++ search.php
@@ -1,21 +1,4 @@
-<?php
-function get_conn_string()
-{
-    $ini = parse_ini_file("php.ini");
-    $host = $ini["dbhost"];
-    $db = $ini["dbname"];
-    $usr = $ini["dbuser"];
-    $pass = $ini["dbpass"];
-    $conn_string = "host=$host port=5432 dbname=$db user=$usr password=$pass";
-    return $conn_string;
-}
-
-// Połączenie z bazą danych PostgreSQL
-$conn = pg_connect(get_conn_string());
-
-// Pobranie wartości wyszukiwania z pola tekstowego
-$search = isset($_GET['search']) ? $_GET['search'] : '';
-
+<!--
 // Zaktualizuj zapytanie SQL z warunkiem WHERE
 $query = "SELECT c.id, c.choroba, w.nazwa AS nazwa_wirusa, c.objawy_ogolne, c.objawy_ju, c.rozpoznanie, c.roznicowanie
           FROM choroba c
@@ -33,26 +16,57 @@ if (!empty($search)) {
 
 $query .= " ORDER BY c.id";
 
-// Pobranie danych z tabeli choroba, wraz z nazwą wirusa
+// Iteracja przez wyniki zapytania i dodanie ich do tablicy wyników
+while ($row = pg_fetch_assoc($result)) {
+    $results[] = $row;
+}
+-->
+<?php
+// Połączenie z bazą danych PostgreSQL
+$conn = pg_connect(get_conn_string());
+
+// Pobranie wartości wyszukiwania z pola tekstowego
+$search = isset($_GET['search']) ? $_GET['search'] : '';
+
+// Zaktualizuj zapytanie SQL z warunkiem WHERE
+$query = "SELECT DISTINCT c.choroba, c.nazwa_wirusa, c.objawy_ogolne, c.objawy_ju, c.rozpoznanie, c.roznicowanie
+          FROM choroba c
+          JOIN wirus w ON c.id_wirus = w.id
+          WHERE c.choroba ILIKE '%" . pg_escape_string($search) . "%'
+          OR c.nazwa_wirusa ILIKE '%" . pg_escape_string($search) . "%'
+          OR c.objawy_ogolne ILIKE '%" . pg_escape_string($search) . "%'
+          OR c.objawy_ju ILIKE '%" . pg_escape_string($search) . "%'
+          OR c.rozpoznanie ILIKE '%" . pg_escape_string($search) . "%'
+          OR c.roznicowanie ILIKE '%" . pg_escape_string($search) . "%'
+          ORDER BY c.choroba";
+
+// Pobranie danych z tabeli choroba
 $result = pg_query($conn, $query);
 
-// Przygotowanie tablicy wyników
+// Utworzenie tablicy wyników
 $results = array();
 
 // Iteracja przez wyniki zapytania i dodanie ich do tablicy wyników
 while ($row = pg_fetch_assoc($result)) {
-    $results[] = $row;
+    $results[] = array(
+        'choroba' => $row['choroba'],
+        'nazwa_wirusa' => $row['nazwa_wirusa'],
+        'objawy_ogolne' => $row['objawy_ogolne'],
+        'objawy_ju' => $row['objawy_ju'],
+        'rozpoznanie' => $row['rozpoznanie'],
+        'roznicowanie' => $row['roznicowanie']
+    );
 }
 
 // Zamknięcie połączenia z bazą danych
 pg_close($conn);
 
-// Zwrócenie tablicy wyników jako odpowiedź w formacie JSON
+// Ustawienie nagłówka Content-Type na application/json
+header('Content-Type: application/json');
+
+// Zwrócenie wyników w formacie JSON
 echo json_encode($results);
 ?>
-
-
-
 
 <!--
 // to można usunąć
